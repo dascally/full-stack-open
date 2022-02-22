38342577b0052b7385494b778836f31df6045690
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-02-21 20:01:20 -0500

    Part 1c: Refactor counter components

--- scratchwork/part1/src/App.js
+++ scratchwork/part1/src/App.js
@@ -1,25 +1,28 @@
 import { useState } from 'react';
 
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
 const App = () => {
   const [counter, setCounter] = useState(0);
 
+  const increaseByOne = () => {
+    setCounter(counter + 1);
+  };
+  const decreaseByOne = () => {
+    setCounter(counter - 1);
+  };
+  const setToZero = () => {
+    setCounter(0);
+  };
+
   return (
     <div>
-      <div>{counter}</div>
-      <button
-        onClick={() => {
-          setCounter(counter + 1);
-        }}
-      >
-        Increment
-      </button>
-      <button
-        onClick={() => {
-          setCounter(0);
-        }}
-      >
-        Zero
-      </button>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text='Increment' />
+      <Button onClick={setToZero} text='Zero' />
+      <Button onClick={decreaseByOne} text='Decrement' />
     </div>
   );
 };
