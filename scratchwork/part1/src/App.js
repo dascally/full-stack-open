48a5878b9b5974f2c6068e5cbe6a239c43e0e2cd
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-02-21 19:32:53 -0500

    Part 1b: Make counter

--- scratchwork/part1/src/App.js
+++ scratchwork/part1/src/App.js
@@ -1,34 +1,13 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}! You are {props.age} years old.
-      </p>
-    </div>
-  );
-};
-
-const Footer = () => {
-  return (
-    <div>
-      Greeting app created by <a href='https://github.com/dascally'>dascally</a>
-      , following the Fullstack Open curriculum.
-    </div>
-  );
-};
+import { useState } from 'react';
 
 const App = () => {
-  const name = 'Peter';
-  const age = 10;
+  const [counter, setCounter] = useState(0);
 
-  return (
-    <>
-      <h1>Greetings!</h1>
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
-      <Footer />
-    </>
-  );
+  setTimeout(() => {
+    setCounter(counter + 1);
+  }, 1000);
+
+  return <div>{counter}</div>;
 };
 
 export default App;
