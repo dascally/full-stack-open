a8d763b7b4f94d7043a57aa63f9faf25ea836dd8
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-02-21 19:44:07 -0500

    Part 1b: Add buttons for counter

--- scratchwork/part1/src/App.js
+++ scratchwork/part1/src/App.js
@@ -3,11 +3,25 @@ import { useState } from 'react';
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  setTimeout(() => {
-    setCounter(counter + 1);
-  }, 1000);
-
-  return <div>{counter}</div>;
+  return (
+    <div>
+      <div>{counter}</div>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          setCounter(0);
+        }}
+      >
+        Zero
+      </button>
+    </div>
+  );
 };
 
 export default App;
