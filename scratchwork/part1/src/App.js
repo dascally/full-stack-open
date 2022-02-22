2249037a355d9f970191ea8bdaca65a3a4d5e13f
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-02-21 20:48:34 -0500

    Part 1d: Make click tracker

--- scratchwork/part1/src/App.js
+++ scratchwork/part1/src/App.js
@@ -1,28 +1,36 @@
 import { useState } from 'react';
 
-const Display = ({ counter }) => <div>{counter}</div>;
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>The app is used by pressing the buttons.</div>;
+  } else {
+    return <div>Button press history: {props.allClicks.join(' ')}</div>;
+  }
+};
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
-  const increaseByOne = () => {
-    setCounter(counter + 1);
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    setLeft(left + 1);
   };
-  const decreaseByOne = () => {
-    setCounter(counter - 1);
-  };
-  const setToZero = () => {
-    setCounter(0);
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
   };
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text='Increment' />
-      <Button onClick={setToZero} text='Zero' />
-      <Button onClick={decreaseByOne} text='Decrement' />
+      {left}
+      <Button onClick={handleLeftClick} text='Left' />
+      <Button onClick={handleRightClick} text='Right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   );
 };
