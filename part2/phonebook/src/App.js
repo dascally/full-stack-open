8e15e4586bfe89d16723f10ba61eb64d918d750f
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-03-08 22:31:12 -0500

    Complete exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,26 +4,39 @@ const Numbers = ({ entries }) => {
   return (
     <ul>
       {entries.map((entry) => (
-        <li key={entry.name}>{entry.name}</li>
+        <li key={entry.name}>
+          {entry.name}: {entry.number}
+        </li>
       ))}
     </ul>
   );
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' },
+  ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const handleNameInput = (evt) => {
     setNewName(evt.target.value);
   };
 
+  const handleNumberInput = (evt) => {
+    setNewNumber(evt.target.value);
+  };
+
   const handleSubmit = (evt) => {
     if (persons.find((person) => person.name === newName)) {
+      // evt.target.elements[0].setCustomValidity(
+      //   `${newName} is already in the phonebook.`
+      // );
       alert(`${newName} is already in the phonebook.`);
     } else {
-      setPersons(persons.concat({ name: newName }));
+      setPersons(persons.concat({ name: newName, number: newNumber }));
       setNewName('');
+      setNewNumber('');
     }
 
     evt.preventDefault();
@@ -35,7 +48,14 @@ const App = () => {
       <form onSubmit={handleSubmit}>
         <div>
           <label>
-            name: <input value={newName} onChange={handleNameInput} />
+            name:{' '}
+            <input type='text' value={newName} onChange={handleNameInput} />
+          </label>
+        </div>
+        <div>
+          <label>
+            number:{' '}
+            <input type='tel' value={newNumber} onChange={handleNumberInput} />
           </label>
         </div>
         <div>
