215c62c214191c9530b81084a6b057d34440d197
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-03-08 22:12:29 -0500

    Complete exercise 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,45 @@
 import { useState } from 'react';
 
+const Numbers = ({ entries }) => {
+  return (
+    <ul>
+      {entries.map((entry) => (
+        <li key={entry.name}>{entry.name}</li>
+      ))}
+    </ul>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
 
+  const handleNameInput = (evt) => {
+    setNewName(evt.target.value);
+  };
+
+  const handleSubmit = (evt) => {
+    setPersons(persons.concat({ name: newName }));
+    setNewName('');
+
+    evt.preventDefault();
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          <label>
+            name: <input value={newName} onChange={handleNameInput} />
+          </label>
         </div>
         <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <Numbers entries={persons} />
     </div>
   );
 };
