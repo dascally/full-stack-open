680a8d45804c2e9d79f374cb41b869d4bd8560fd
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-03-11 00:41:06 -0500

    Complete exercise 2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -5,6 +5,7 @@ import CountryResults from './components/CountryResults.js';
 function App() {
   const [countries, setCountries] = useState([]);
   const [countryQuery, setCountryQuery] = useState('');
+  const [specifiedCountry, setSpecifiedCountry] = useState(null);
 
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all').then((res) => {
@@ -22,13 +23,19 @@ function App() {
             value={countryQuery}
             onChange={(evt) => {
               setCountryQuery(evt.target.value);
+              setSpecifiedCountry(null);
             }}
             id='query'
           />
         </label>
       </p>
 
-      <CountryResults countries={countries} query={countryQuery} />
+      <CountryResults
+        countries={countries}
+        query={countryQuery}
+        specifiedCountry={specifiedCountry}
+        setSpecifiedCountry={setSpecifiedCountry}
+      />
     </>
   );
 }
--- part2/countries/src/components/CountryResults.js
+++ part2/countries/src/components/CountryResults.js
@@ -1,6 +1,11 @@
 import CountryData from './CountryData.js';
 
-const CountryResults = ({ countries, query }) => {
+const CountryResults = ({
+  countries,
+  query,
+  specifiedCountry,
+  setSpecifiedCountry,
+}) => {
   if (query === '') return null;
 
   const searchResults = countries.filter((country) => {
@@ -9,13 +14,20 @@ const CountryResults = ({ countries, query }) => {
     return normalizedName.includes(normalizedQuery);
   });
 
-  if (searchResults.length > 10) {
+  if (specifiedCountry) {
+    return <CountryData country={specifiedCountry} />;
+  } else if (searchResults.length > 10) {
     return <p>Too many matches; specify another filter.</p>;
   } else if (searchResults.length > 1) {
     return (
       <ul>
         {searchResults.map((country) => (
-          <li key={country.cca3}>{country.name.common}</li>
+          <li key={country.cca3}>
+            {country.name.common}{' '}
+            <button type='button' onClick={() => setSpecifiedCountry(country)}>
+              Show
+            </button>
+          </li>
         ))}
       </ul>
     );
