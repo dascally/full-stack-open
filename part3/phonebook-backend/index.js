46fd882a1974277dbc18267d03726e55a2ca0950
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-03-17 01:00:37 -0400

    Complete exercise 3.16

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -7,29 +7,6 @@ morgan.token('data', (req, res) => JSON.stringify(req.body));
 
 const PORT = process.env.PORT ?? 3001;
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
 const app = express();
 
 app.listen(PORT, () => {
@@ -50,19 +27,23 @@ app.use('/api', cors());
 app.use('/api', express.json());
 
 app.get('/api/persons', (req, res) => {
-  Person.find({}).then((persons) => {
-    res.json(persons);
-  });
+  Person.find({})
+    .then((persons) => {
+      res.json(persons);
+    })
+    .catch(next);
 });
 
 app.get('/api/persons/:id', (req, res) => {
-  Person.findById(req.params.id).then((person) => {
-    res.json(person);
-  });
+  Person.findById(req.params.id)
+    .then((person) => {
+      res.json(person);
+    })
+    .catch(next);
 });
 
 app.post('/api/persons', morgan(':data'));
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   if (!req.body.name || !req.body.number) {
     res.statusCode = 400;
     res.json({ error: 'Name or number is missing.' });
@@ -74,21 +55,37 @@ app.post('/api/persons', (req, res) => {
     number: req.body.number,
   });
 
-  newPerson.save().then((person) => {
-    res.json(person);
-  });
+  newPerson
+    .save()
+    .then((person) => {
+      res.json(person);
+    })
+    .catch(next);
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-  Person.findByIdAndRemove(req.params.id).then((person) => {
-    if (person) {
-      res.statusCode = 204;
-      res.end();
-    } else {
-      res.statusCode = 404;
-      res.end();
-    }
-  });
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.statusCode = 204;
+        res.end();
+      } else {
+        res.statusCode = 404;
+        res.end();
+      }
+    })
+    .catch(next);
 });
 
 app.use(express.static('build'));
+
+app.use((err, req, res, next) => {
+  console.error(err.message);
+
+  if (err.name === 'CastError') {
+    res.statusCode = 400;
+    res.send({ error: 'Malformed ID.' });
+  }
+
+  next(err);
+});
