597e3c592962605bb806e1dbf7b84034b85e20b0
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-03-16 22:56:14 -0400

    Part 3c, before exercise 3.13

--- scratchwork/part3/a/index.js
+++ scratchwork/part3/a/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const Note = require('./models/note.js');
 
 const app = express();
 
@@ -46,25 +47,16 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes);
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
 });
 
 app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
+  Note.findById(req.params.id).then((note) => {
     res.json(note);
-  } else {
-    res.status(404).end();
-  }
+  });
 });
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-
-  return maxId + 1;
-};
 
 app.post('/api/notes', (req, res) => {
   const body = req.body;
@@ -73,16 +65,15 @@ app.post('/api/notes', (req, res) => {
     return res.status(400).json({ error: 'Content missing.' });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important ?? false,
     date: new Date(),
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  res.json(note);
+  note.save().then((savedNote) => {
+    res.json(savedNote);
+  });
 });
 
 app.delete('/api/notes/:id', (req, res) => {
