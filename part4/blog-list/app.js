33abe917b7c557b307f66056644bb8526f8889d0
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-03-21 21:09:27 -0400

    blog-list: Move server config to a separate file

---
+++ part4/blog-list/app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+const blogsRouter = require('./routes/blogs.js');
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI).then(() => {
+  console.log('Connected to database.');
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,22 +1,10 @@
 require('dotenv').config();
 const http = require('http');
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const app = express();
-const blogsRouter = require('./routes/blogs.js');
-
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl).then(() => {
-  console.log('Connected to database.');
-});
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/blogs', blogsRouter);
+const app = require('./app.js');
 
 const PORT = process.env.PORT ?? 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}.`);
 });
