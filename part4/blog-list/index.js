8ad9564737248d25a46469e4bf4d44a374cea9fd
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-03-21 20:48:55 -0400

    blog-list: move Blog schema to separate file

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -4,17 +4,12 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-const Blog = mongoose.model('Blog', blogSchema);
+const Blog = require('./models/blog.js');
 
 const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl);
+mongoose.connect(mongoUrl).then(() => {
+  console.log('Connected to database.');
+});
 
 app.use(cors());
 app.use(express.json());
