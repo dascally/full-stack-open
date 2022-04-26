22907763d835200dbc89e97e3c8ebd0af3981124
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-04-26 13:56:03 -0400

    Complete exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const blogStyle = {
+    border: 'solid 1px black',
+    marginBottom: '0.25em',
+    padding: '0.1em 0.25em',
+  };
+  const blogDetailsStyle = {
+    display: expanded ? null : 'none',
+  };
+
+  const handleToggleClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}, by {blog.author}{' '}
+        <button type='button' onClick={handleToggleClick}>
+          {expanded ? 'Hide' : 'Show'}
+        </button>
+      </div>
+      <div style={blogDetailsStyle}>
+        URL: {blog.url}
+        <br />
+        Likes: {blog.likes}
+        <br />
+        User: {blog.user.name}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
