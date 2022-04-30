cb64c5966954e05fc65e9797ca60d3c7437534a7
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-04-30 19:37:13 -0400

    Complete exercise 5.18

--- part5/bloglist-frontend/cypress/integration/bloglist_spec.js
+++ part5/bloglist-frontend/cypress/integration/bloglist_spec.js
@@ -1,7 +1,10 @@
 /// <reference types="Cypress" />
 
+const testUser = Cypress.env('user');
+
 beforeEach(() => {
   cy.request('POST', 'http://localhost:3003/api/testing/reset');
+  cy.request('POST', 'http://localhost:3003/api/users', testUser);
   cy.visit('/');
 });
 
@@ -9,4 +12,22 @@ it('shows login form', () => {
   cy.findByLabelText(/username/i).should('exist');
   cy.findByLabelText(/password/i).should('exist');
   cy.findByRole('button', { name: /login/i }).should('exist');
+});
+
+describe('Login', () => {
+  it('succeeds with correct credentials', () => {
+    cy.findByLabelText(/username/i).type(testUser.username);
+    cy.findByLabelText(/password/i).type(testUser.password);
+    cy.findByRole('button', { name: /login/i }).click();
+
+    cy.contains(`${testUser.name} is logged in.`).should('exist');
+  });
+
+  it('fails with incorrect credentials', () => {
+    cy.findByLabelText(/username/i).type(testUser.username);
+    cy.findByLabelText(/password/i).type('wrong password');
+    cy.findByRole('button', { name: /login/i }).click();
+
+    cy.contains(/invalid credentials/i);
+  });
 });
