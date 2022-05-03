bace9035b27a01879068d257e531de3bcb5089ed
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-05-02 21:22:20 -0400

    Complete exercise 6.12

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -7,10 +7,10 @@ import {
 
 const AnecdoteList = () => {
   const anecdotes = useSelector((state) => state.anecdotes);
+  const filter = useSelector((state) => state.filter);
   const dispatch = useDispatch();
 
   const vote = (id) => {
-    console.log('vote', id);
     dispatch(voteForAnecdote(id));
     dispatch(
       setNotification(
@@ -26,15 +26,17 @@ const AnecdoteList = () => {
 
   return anecdotes
     .sort((a, b) => b.votes - a.votes)
-    .map((anecdote) => (
-      <div key={anecdote.id}>
-        <div>{anecdote.content}</div>
-        <div>
-          has {anecdote.votes}
-          <button onClick={() => vote(anecdote.id)}>vote</button>
+    .map((anecdote) =>
+      anecdote.content.toLowerCase().includes(filter.toLowerCase()) ? (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
         </div>
-      </div>
-    ));
+      ) : null
+    );
 };
 
 export default AnecdoteList;
