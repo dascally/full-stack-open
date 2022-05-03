71f97328ac97f3f0f6977472a829e0078e914138
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-05-02 23:14:34 -0400

    Part 6c, before exercise 6.13

--- scratchwork/part6/a/src/App.js
+++ scratchwork/part6/a/src/App.js
@@ -1,8 +1,20 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import * as noteService from './services/notes';
+import { setNotes } from './reducers/noteReducer';
 import NewNote from './components/NewNote';
 import Notes from './components/Notes';
 import VisibilityFilter from './components/VisibilityFilter';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    noteService.getAll().then((notes) => {
+      dispatch(setNotes(notes));
+    });
+  }, [dispatch]);
+
   return (
     <div>
       <NewNote />
--- scratchwork/part6/a/src/components/NewNote.js
+++ scratchwork/part6/a/src/components/NewNote.js
@@ -1,16 +1,18 @@
 import { createNote } from '../reducers/noteReducer';
 import { useDispatch } from 'react-redux';
+import * as noteService from '../services/notes';
 
 const NewNote = (props) => {
   const dispatch = useDispatch();
 
-  const addNote = (evt) => {
+  const addNote = async (evt) => {
     evt.preventDefault();
 
     const content = evt.target.elements.note.value;
     evt.target.elements.note.value = '';
 
-    dispatch(createNote(content));
+    const newNote = await noteService.createNew(content);
+    dispatch(createNote(newNote));
   };
 
   return (
