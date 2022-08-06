a1e65037fd57eeb044e9109d0367170fe8e8291a
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-08-06 15:30:13 -0400

    Complete exercise 9.5

--- part9/exercise/bmiCalculator.ts
+++ part9/exercise/bmiCalculator.ts
@@ -1,19 +1,29 @@
-function calculateBmi(heightInCm: number, weightInKg: number) {
-  const bmi = weightInKg / (heightInCm / 100) ** 2;
-
-  if (bmi < 18.5) {
-    console.log('Underweight');
-  } else if (bmi < 25.0) {
-    console.log('Normal (healthy weight)');
-  } else if (bmi < 30.0) {
-    console.log('Overweight');
-  } else {
-    console.log('Obese');
-  }
-
-  return bmi;
+interface BmiResult {
+  bmi: number;
+  message: string;
 }
 
+export default function calculateBmi(
+  heightInCm: number,
+  weightInKg: number
+): BmiResult {
+  const bmi = weightInKg / (heightInCm / 100) ** 2;
+
+  let message;
+  if (bmi < 18.5) {
+    message = 'Underweight';
+  } else if (bmi < 25.0) {
+    message = 'Normal (healthy weight)';
+  } else if (bmi < 30.0) {
+    message = 'Overweight';
+  } else {
+    message = 'Obese';
+  }
+
+  return { bmi, message };
+}
+
+/*
 function parseArgsForBmi(args: typeof process.argv) {
   if (args.length < 4) throw new Error('Too few arguments.');
   if (args.length > 4) throw new Error('Too many arguments.');
@@ -36,4 +46,5 @@ try {
     console.error(err.message);
   }
 }
+*/
 // console.log(calculateBmi(180, 74));
--- part9/exercise/index.ts
+++ part9/exercise/index.ts
@@ -1,11 +1,35 @@
-import express from 'express';
+import express, { type NextFunction } from 'express';
+import calculateBmi from './bmiCalculator';
 
 const PORT = 3003;
 const app = express();
 
-app.get('/hello', (_req, res) => {
-  res.send('<p>Hello, Full Stack!</p>');
-});
+app
+  .get('/hello', (_req, res) => {
+    res.send('<p>Hello, Full Stack!</p>');
+  })
+  .get('/bmi', (req, res) => {
+    let height: number | typeof req.query['height'] = req.query.height;
+    let weight: number | typeof req.query['weight'] = req.query.weight;
+
+    if (!height || !weight) {
+      throw new Error('Must provide a height and weight.');
+    }
+
+    height = +height;
+    weight = +weight;
+
+    if (isNaN(height) || isNaN(weight)) {
+      throw new Error('Invalid height or weight provided.');
+    }
+
+    const bmiResult = calculateBmi(height, weight);
+
+    res.json({ weight, height, bmi: bmiResult.message });
+  })
+  .use((err: Error, _req: any, res: any, _next: NextFunction) => {
+    res.status(400).json({ error: err.message });
+  });
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}.`);
