cbc7b87e6adbdb6b877145f0d316829f18e621f6
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-08-06 17:49:41 -0400

    Part 9b up to exercise 9.6

--- scratchwork/part9/index.ts
+++ scratchwork/part9/index.ts
@@ -1,11 +1,25 @@
 import express from 'express';
+import { calculator } from './calculator';
+
+const PORT = 3003;
 const app = express();
 
 app.get('/ping', (_req, res) => {
   res.send('pong');
 });
 
-const PORT = 3003;
+app.post('/calculate', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { value1, value2, op } = req.body;
+
+  if (!value1 || !value2 || isNaN(+value1) || isNaN(+value2)) {
+    return res.status(400).send({ error: 'Must provide two numbers.' });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const result = calculator(+value1, +value2, op);
+  return res.send(result);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}.`);
