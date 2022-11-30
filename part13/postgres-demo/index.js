0072da35645e03793ee538fc0ce60c699c7ca2bc
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-11-30 18:36:38 -0500

    Part 13b up to exercise 13.5

--- part13/postgres-demo/index.js
+++ part13/postgres-demo/index.js
@@ -1,77 +1,21 @@
-require('dotenv').config();
-const { Sequelize, Model, DataTypes } = require('sequelize');
 const express = require('express');
 
+const { PORT } = require('./util/config');
+const { connectToDatabase } = require('./util/db');
+
 const app = express();
-const sequelize = new Sequelize(process.env.DATABASE_URL);
-const PORT = process.env.PORT ?? 3001;
 
-class Note extends Model {}
-Note.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    important: {
-      type: DataTypes.BOOLEAN,
-    },
-    date: {
-      type: DataTypes.DATE,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: 'note',
-  }
-);
-Note.sync();
+const notesRouter = require('./controllers/notes');
 
-app
-  .get('/api/notes', async (req, res) => {
-    const notes = await Note.findAll();
+app.use(express.json());
 
-    console.log(JSON.stringify(notes, null, 2));
+app.use('/api/notes', notesRouter);
 
-    res.json(notes);
-  })
-  .get('/api/notes/:id', async (req, res) => {
-    const note = await Note.findByPk(req.params.id);
-    if (note) {
-      console.log(note.toJSON());
-      res.json(note);
-    } else {
-      res.status(404).end();
-    }
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}.`);
   });
+};
 
-app.post('/api/notes', async (req, res) => {
-  try {
-    const note = await Note.create(req.body);
-    res.json(note);
-  } catch (err) {
-    res.status(400).json({ error: err });
-  }
-});
-
-app.put('/api/notes/:id', async (req, res) => {
-  const note = await Note.findByPk(req.params.id);
-  if (note) {
-    note.important = req.body.important;
-    await note.save();
-    res.json(note);
-  } else {
-    res.status(404).end();
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}.`);
-});
+start();
