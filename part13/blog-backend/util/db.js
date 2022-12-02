74d19e78af302784a05b4b41c8341331eb5cf6b2
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-12-02 16:02:32 -0500

    Complete exercise 13.17

--- part13/blog-backend/util/db.js
+++ part13/blog-backend/util/db.js
@@ -1,18 +1,41 @@
 const Sequelize = require('sequelize');
 const { DB_URL } = require('./config');
+const { Umzug, SequelizeStorage } = require('umzug');
 
 const sequelize = new Sequelize(DB_URL);
 
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
+    await runMigrations();
     console.log('Connected to the database.');
   } catch (err) {
-    console.log('Failed to connect to the database.');
+    console.log('Failed to connect to the database.', err);
     return process.exit(1);
   }
 
   return null;
 };
 
-module.exports = { connectToDatabase, sequelize };
+const migrationConf = {
+  migrations: { glob: 'migrations/*.js' },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+};
+
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf);
+  const migrations = await migrator.up();
+  console.log('Migrations are up to date.', {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
+const rollBackMigration = async () => {
+  await sequelize.authenticate();
+  const migrator = new Umzug(migrationConf);
+  await migrator.down();
+};
+
+module.exports = { connectToDatabase, sequelize, rollBackMigration };
