9ef40926474e99fd72fc2b37c02f26d4e42c4ad1
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-12-02 18:51:07 -0500

    Complete exercise 13.19

--- part13/blog-backend/models/index.js
+++ part13/blog-backend/models/index.js
@@ -1,13 +1,17 @@
 const Blog = require('./blog');
 const User = require('./user');
+const ReadingList = require('./reading_list');
+const ReadingListContents = require('./reading_list_contents');
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
-// Blog.sync({ alter: true });
-// User.sync({ alter: true });
+Blog.belongsToMany(ReadingList, { through: ReadingListContents });
+ReadingList.belongsToMany(Blog, { through: ReadingListContents });
 
 module.exports = {
   Blog,
   User,
+  ReadingList,
+  ReadingListContents,
 };
